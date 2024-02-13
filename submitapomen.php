6f6fc2f4188903c5b31a5117caf3f16ef4a4d446
Projects <159575309+sappaya64@users.noreply.github.com>
2024-02-13 15:45:31 +0700

    กก

--- submitapomen.php
+++ submitapomen.php
@@ -1,45 +1,36 @@
 <?php
-    session_start();
- $servername = "localhost";
- $username = "root";
- $password = "";
- $dbname = "fund";
- 
- // สร้างการเชื่อมต่อ
- $conn = new mysqli($servername, $username, $password, $dbname);
- 
+session_start();
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "fund";
+
+// สร้างการเชื่อมต่อ
+$conn = new mysqli($servername, $username, $password, $dbname);
+
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    die("การเชื่อมต่อล้มเหลว: " . $conn->connect_error);
 }
+
 if (isset($_SESSION['user_login'])) {
     $user_id = $_SESSION['user_login'];
     $stmt = $conn->query("SELECT * FROM patien WHERE id = $user_id");
 }
 
-if (isset($_POST['summit'])) {
+if (isset($_POST['submit'])) { // ปรับเปลี่ยนจาก 'ยืนยัน' เป็นชื่อของปุ่ม submit ในฟอร์ม
     $doctor = $_POST['doctor'];
     $info = $_POST['infomation'];
-   
     $date = $_POST['date'];
     $time = $_POST['timeInput'];
-
     $email = $_POST['email'];
     $patient = $_POST['patient'];
     $state = $_POST['state'];
-   
-
-    state
-
-    
-
-    $sql = "INSERT INTO appointmen (user_id,doctor_id, information,appointment_date,appointment_time, email,patient,state) VALUES ('$user_id' ,'$doctor', '$info',
-    '$date',' $time','$email','$patient','$state')";
-
+    $sql = "INSERT INTO appointmen (user_id, doctor_id, information, appointment_date, appointment_time, email, patient, state) VALUES ('$user_id', '$doctor', '$info', '$date', '$time', '$email', '$patient', '$state')";
 
     if ($conn->query($sql) === TRUE) {
-        echo "Review submitted successfully!";
+        echo "บันทึกการนัดหมายเรียบร้อยแล้ว!";
     } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
+        echo "ข้อผิดพลาด: " . $sql . "<br>" . $conn->error;
     }
 }
 
