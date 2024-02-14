8aca6a365eeab7dc79a3c66f1674c815086c69d8
Projects <159575309+sappaya64@users.noreply.github.com>
2024-02-14 21:15:02 +0700

    Update dash_produc.php

--- dashboard/dash_produc.php
+++ dashboard/dash_produc.php
@@ -81,6 +81,10 @@ $conn->close();
     <!-- Set the width and height directly -->
     <div id="chart-container">
         <div id="productChart"></div>
+        <div style="position: relative; width: 560px; height: 500px; padding: 0px; margin: 0px; border-width: 0px; cursor: default; left: 0;">
+    <canvas data-zr-dom-id="zr_0" width="560" height="500" style="position: absolute; left: 0px; top: 0px; width: 560px; height: 500px; user-select: none; -webkit-tap-highlight-color: rgba(0, 0, 0, 0); padding: 0px; margin: 0px; border-width: 0px;"></canvas>
+</div>
+
         
         <div id="legend"></div>
     </div>
