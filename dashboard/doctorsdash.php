41d8426ae3f6d87954f511d0ddf792402b651864
KKStable13 <143015378+KKStable13@users.noreply.github.com>
2024-02-17 22:46:23 +0700

    Update doctorsdash.php
    เพิ่มลิ้งCSSแล้วทำให้ icon โผล่ตรงแท็บเทา รวมถึงFont

--- dashboard/doctorsdash.php
+++ dashboard/doctorsdash.php
@@ -28,6 +28,10 @@ try {
     <title>ข้อมูลหมอ</title>
     <!-- ลิงก์ CSS ของ Bootstrap -->
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Bootstrap CSS -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Font Awesome CSS -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
     <style>
         .table {
             border: 1px solid #dee2e6;
